7144abf25eb5ea509dd324d227668911c95f77e9
valerianhubgit <VKurowski@gmx.de>
2026-03-16 11:02:10 +0100

    Game's running with separate functions main, init_grid, draw, update

---
+++ GOF.py
@@ -0,0 +1,65 @@
+import time
+import pygame
+import numpy as np
+
+#Set Variables
+width = 1000
+height = 1000
+FPS = 60
+size = 10
+alive_color = (210, 210, 210)
+unalive_color= (25, 25, 25)
+
+def draw(screen, cells):
+    screen.fill(unalive_color)
+    for row, col in np.ndindex(cells.shape):
+        if cells[row,col]:
+            pygame.draw.rect(screen, alive_color, (row*size, col*size, size-1, size-1))
+
+def update(cells):
+    updated_cells=np.zeros((cells.shape[0], cells.shape[1]))
+    for row, col in np.ndindex(cells.shape):
+        no_neighbours=np.sum(cells[row-1:row+2,col-1:col+2])-cells[row, col]
+        #Remains alive?
+        if cells[(row,col)] and no_neighbours in [2,3]:
+            updated_cells[(row, col)] = 1
+        #Rises from unalive?
+        elif (not cells[(row,col)]) and no_neighbours == 3:
+            updated_cells[(row,col)] = 1
+    return updated_cells
+
+def init_grid(rows, cols, random=True):
+    if random:
+        return np.random.choice([0,1], size=(rows, cols), p=[0.8, 0.2])
+    return  np.zeros((rows, cols)) 
+
+#kreiere Pygame-Screen mit Grid und Klickbaren Feldern zum Alive/Unalive - Triggern
+def main():
+    #alive cells get a 1, unalive cells get 0, initialize randomly
+    cells=init_grid(width//size, height//size)
+
+    pygame.init()
+    screen=pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Game of Life")
+
+    clock=pygame.time.Clock()
+
+    running=True
+    #öffne Fenster
+    while running:
+        #Core Mechanic
+        clock.tick(FPS)
+        cells=update(cells)
+        draw(screen, cells)        
+        pygame.display.update()
+        
+        #Inputs
+        for event in pygame.event.get():
+            #Schließen
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                return
+
+#apparently notwendig um nicht bei Import pygame ständig auszuführen
+if __name__=="__main__":
+    main()
